a002d7138577c3f453589234f52998ac618d9dbb
jamessykwan <31811580+jamessykwan@users.noreply.github.com>
2017-11-12 07:01:06 -0800

    7:00 front-end James

--- databaselistener.js
+++ databaselistener.js
@@ -1,8 +1,9 @@
 let database = firebase.database();
+console.log(database);
 //Electricity Used
 database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#elecUsed');
+	var element = document.querySelector('#energy');
 	var repEl = document.createElement('p');
 	repEl.id = "elecUsed";
 	repEl.innerHTML =  snapshot.val() + "Electricity Used";
@@ -12,7 +13,7 @@ database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", 
 //Recommended Usage
 database.ref("Users/CodeDayUser/Stats/Electricity/RecommendedUsage").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#recUsed');
+	var element = document.querySelector('#recommendedenergy');
 	var repEl = document.createElement('p');
 	repEl.id = "recUsed";
 	repEl.innerHTML =  snapshot.val() + "RecommendedUsage";
@@ -22,7 +23,7 @@ database.ref("Users/CodeDayUser/Stats/Electricity/RecommendedUsage").on("value",
 //Electricity Savings
 database.ref("Users/CodeDayUser/Stats/Electricity/Savings").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#savingsElec');
+	var element = document.querySelector('#energysavings');
 	var repEl = document.createElement('p');
 	repEl.id = "savingElec";
 	repEl.innerHTML =  snapshot.val() + "Savings";
@@ -30,9 +31,9 @@ database.ref("Users/CodeDayUser/Stats/Electricity/Savings").on("value", function
 
 }, (error) => console.log(error));
 //Waste Produced
-database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", function(snapshot) {
+database.ref("Users/CodeDayUser/Stats/Waste/WasteProduced").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#elecUsed');
+	var element = document.querySelector('#waste');
 	var repEl = document.createElement('p');
 	repEl.id = "elecUsed";
 	repEl.innerHTML =  snapshot.val() + "Electricity Used";
@@ -42,7 +43,7 @@ database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", 
 //Recommended Waste
 database.ref("Users/CodeDayUser/Stats/Waste/RecommendedWaste").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#recWaste');
+	var element = document.querySelector('#recommendedwaste');
 	var repEl = document.createElement('p');
 	repEl.id = "recWaste";
 	repEl.innerHTML =  snapshot.val() + "Recommended Waste";
@@ -52,7 +53,7 @@ database.ref("Users/CodeDayUser/Stats/Waste/RecommendedWaste").on("value", funct
 //WaterUsed
 database.ref("Users/CodeDayUser/Stats/Water/WaterUsed").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#waterUsed');
+	var element = document.querySelector('#water');
 	var repEl = document.createElement('p');
 	repEl.id = "waterUsed";
 	repEl.innerHTML =  snapshot.val() + "Water Used";
@@ -62,7 +63,7 @@ database.ref("Users/CodeDayUser/Stats/Water/WaterUsed").on("value", function(sna
 //Recommended Water Usage
 database.ref("Users/CodeDayUser/Stats/Water/RecommendedUsage").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#recWater');
+	var element = document.querySelector('#recommendedwater');
 	var repEl = document.createElement('p');
 	repEl.id = "recWater";
 	repEl.innerHTML =  snapshot.val() + "Recommended Water";
@@ -72,7 +73,7 @@ database.ref("Users/CodeDayUser/Stats/Water/RecommendedUsage").on("value", funct
 //Water Savings
 database.ref("Users/CodeDayUser/Stats/Water/Savings").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#waterSavings');
+	var element = document.querySelector('#watersavings');
 	var repEl = document.createElement('p');
 	repEl.id = "waterSavings";
 	repEl.innerHTML =  snapshot.val() + "Water Savings";
