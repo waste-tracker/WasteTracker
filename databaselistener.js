cdbda6890579a342e906e2e55c5967d2dfccecd8
Jacob Chazen <jchazen@ucsd.edu>
2017-11-12 03:42:51 -0800

    3:42 databaselistener.js v2 Jake

--- databaselistener.js
+++ databaselistener.js
@@ -1,38 +1,82 @@
 let database = firebase.database();
-
-database.ref("Users/CodeDayUser/gal_waterUsed").on("value", function(snapshot) {
+//Electricity Used
+database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#water');
+	var element = document.querySelector('#elecUsed');
 	var repEl = document.createElement('p');
-	repEl.id = "water";
-	repEl.innerHTML = "Water Used: " + snapshot.val() + "Gallons";
+	repEl.id = "elecUsed";
+	repEl.innerHTML =  snapshot.val() + "Electricity Used";
 	element.parentNode.replaceChild(repEl, element);
 
 }, (error) => console.log(error));
-console.log(document);
-
-database.ref("Users/CodeDayUser/kWh_powerUsed").on("value", function(snapshot) {
+//Recommended Usage
+database.ref("Users/CodeDayUser/Stats/Electricity/RecommendedUsage").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#energy');
+	var element = document.querySelector('#recUsed');
 	var repEl = document.createElement('p');
-	repEl.id = "energy";
-	repEl.innerHTML = "Energy Used: " + snapshot.val() + "kWh";
+	repEl.id = "recUsed";
+	repEl.innerHTML =  snapshot.val() + "RecommendedUsage";
 	element.parentNode.replaceChild(repEl, element);
 
 }, (error) => console.log(error));
-console.log(document);
-
-database.ref("Users/CodeDayUser/lb_wasteProduced").on("value", function(snapshot) {
+//Electricity Savings
+database.ref("Users/CodeDayUser/Stats/Electricity/Savings").on("value", function(snapshot) {
 	console.log(snapshot.val());
-	var element = document.querySelector('#waste');
+	var element = document.querySelector('#savingsElec');
 	var repEl = document.createElement('p');
-	repEl.id = "wasteProduced";
-	repEl.innerHTML = "Waste Produced: " + snapshot.val() + "lbs";
+	repEl.id = "savingElec";
+	repEl.innerHTML =  snapshot.val() + "Savings";
+	element.parentNode.replaceChild(repEl, element);
+
+}, (error) => console.log(error));
+//Waste Produced
+database.ref("Users/CodeDayUser/Stats/Electricity/ElectricityUsed").on("value", function(snapshot) {
+	console.log(snapshot.val());
+	var element = document.querySelector('#elecUsed');
+	var repEl = document.createElement('p');
+	repEl.id = "elecUsed";
+	repEl.innerHTML =  snapshot.val() + "Electricity Used";
+	element.parentNode.replaceChild(repEl, element);
+
+}, (error) => console.log(error));
+//Recommended Waste
+database.ref("Users/CodeDayUser/Stats/Waste/RecommendedWaste").on("value", function(snapshot) {
+	console.log(snapshot.val());
+	var element = document.querySelector('#recWaste');
+	var repEl = document.createElement('p');
+	repEl.id = "recWaste";
+	repEl.innerHTML =  snapshot.val() + "Recommended Waste";
+	element.parentNode.replaceChild(repEl, element);
+
+}, (error) => console.log(error));
+//WaterUsed
+database.ref("Users/CodeDayUser/Stats/Water/WaterUsed").on("value", function(snapshot) {
+	console.log(snapshot.val());
+	var element = document.querySelector('#waterUsed');
+	var repEl = document.createElement('p');
+	repEl.id = "waterUsed";
+	repEl.innerHTML =  snapshot.val() + "Water Used";
+	element.parentNode.replaceChild(repEl, element);
+
+}, (error) => console.log(error));
+//Recommended Water Usage
+database.ref("Users/CodeDayUser/Stats/Water/RecommendedUsage").on("value", function(snapshot) {
+	console.log(snapshot.val());
+	var element = document.querySelector('#recWater');
+	var repEl = document.createElement('p');
+	repEl.id = "recWater";
+	repEl.innerHTML =  snapshot.val() + "Recommended Water";
+	element.parentNode.replaceChild(repEl, element);
+
+}, (error) => console.log(error));
+//Water Savings
+database.ref("Users/CodeDayUser/Stats/Water/Savings").on("value", function(snapshot) {
+	console.log(snapshot.val());
+	var element = document.querySelector('#waterSavings');
+	var repEl = document.createElement('p');
+	repEl.id = "waterSavings";
+	repEl.innerHTML =  snapshot.val() + "Water Savings";
 	element.parentNode.replaceChild(repEl, element);
 
 }, (error) => console.log(error));
 
-
-function power_Used(value){
- this.value = value;
-}
